0dbae8ceb0344ec9633b4f9e18184affbb82819b
89saycode <126534239+89saycode@users.noreply.github.com>
2023-10-25 12:49:09 +0800

    update file html

--- js/product-navbar.js
+++ js/product-navbar.js
@@ -1,12 +1,44 @@
 function createProductHTML(category) {
-    return `
-        <li class="nav-item">${category}</li>
+  return `
+        <li class="nav-item ${category.className}">${category.name}</li>
       `;
-  }
-  
-  var categorys = ['蔬菜', '水果', '粮油', '服装', '零食', '调料', '酒水', '饮料'];
-  
-  var productHTML = categorys.map(createProductHTML).join("");
-  
-  document.getElementsByClassName("navbar")[0].innerHTML = productHTML;
-  
+}
+
+var categorys = [
+  {
+    name: "全部",
+    className: "hover",
+  },
+  {
+    name: "蔬菜",
+    className: "",
+  },
+  {
+    name: "水果",
+    className: "",
+  },
+  {
+    name: "粮油",
+    className: "",
+  },
+  {
+    name: "服装",
+    className: "",
+  },
+  {
+    name: "调料",
+    className: "",
+  },
+  {
+    name: "酒水",
+    className: "",
+  },
+  {
+    name: "饮料",
+    className: "",
+  },
+];
+
+var productHTML = categorys.map(createProductHTML).join("");
+
+document.getElementsByClassName("navbar")[0].innerHTML = productHTML;
